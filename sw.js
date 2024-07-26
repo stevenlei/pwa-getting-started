da10936b72843cdd80229928b67480e3ab4996de
Steven Lei <stevenplus@gmail.com>
2024-07-27 00:36:09 +0800

    cache first strategy - network as fallback

--- sw.js
+++ sw.js
@@ -14,14 +14,23 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
-        });
+    caches.match(event.request).then((response) => {
+      if (response) {
         return response;
-      })
-      .catch(() => caches.match(event.request))
+      }
+
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200) {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
   );
 });
