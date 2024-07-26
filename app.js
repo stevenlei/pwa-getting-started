ded46496ddd8874913fdb05e5fb0723bafcbdc30
Steven Lei <stevenplus@gmail.com>
2024-07-26 23:44:06 +0800

    register service worker

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 window.addEventListener("load", function () {
   loadPosts();
+
+  // Register service worker
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  }
 });
 
 async function loadPosts() {
