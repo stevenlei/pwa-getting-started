1953946aabc7c358ec2a32d0308673c198ef660d
Steven Lei <stevenplus@gmail.com>
2024-07-27 00:47:09 +0800

    a fallback example

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
 // sw.js (Service Worker)
 
 const CACHE_NAME = "pwa_demo_cache";
-const urlsToCache = ["/", "/index.html", "/style.css", "/app.js"];
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/fallback.json",
+];
 
 async function preCache() {
   const cache = await caches.open(CACHE_NAME);
@@ -14,8 +20,14 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches
+          .match(event.request)
+          .then((response) => response || caches.match("/fallback.json"));
+      })
   );
 });
