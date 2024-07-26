ed4190a6e089df5ad0d72486628a6bfa76f3bc68
Steven Lei <stevenplus@gmail.com>
2024-07-27 00:28:50 +0800

    cache website main assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js (Service Worker)
+
+const CACHE_NAME = "pwa_demo_cache";
+const urlsToCache = ["/", "/index.html", "/style.css", "/app.js"];
+
+async function preCache() {
+  const cache = await caches.open(CACHE_NAME);
+  return cache.addAll(urlsToCache);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(preCache());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
